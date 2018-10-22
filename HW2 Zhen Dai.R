29aa3c2454c8d705584ca6291d6e0a330cdcfd47
Eric-Zhen <44211722+Eric-Zhen@users.noreply.github.com>
2018-10-21 22:32:03 -0500

    function plot
    New script replacing the ploting part by calling a single function called plot_summary

--- HW2 Zhen Dai.R
+++ HW2 Zhen Dai.R
@@ -69,23 +69,26 @@ acceptance = 1-mean(duplicated(chain[-(1:burnIn),]))
 
 ### Summary: #######################
 
-par(mfrow = c(2,3))
-hist(chain[-(1:burnIn),1],nclass=30, , main="Posterior of a", xlab="True value = red line" )
-abline(v = mean(chain[-(1:burnIn),1]))
-abline(v = trueA, col="red" )
-hist(chain[-(1:burnIn),2],nclass=30, main="Posterior of b", xlab="True value = red line")
-abline(v = mean(chain[-(1:burnIn),2]))
-abline(v = trueB, col="red" )
-hist(chain[-(1:burnIn),3],nclass=30, main="Posterior of sd", xlab="True value = red line")
-abline(v = mean(chain[-(1:burnIn),3]) )
-abline(v = trueSd, col="red" )
-plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of a", )
-abline(h = trueA, col="red" )
-plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of b", )
-abline(h = trueB, col="red" )
-plot(chain[-(1:burnIn),3], type = "l", xlab="True value = red line" , main = "Chain values of sd", )
-abline(h = trueSd, col="red" )
+plot_summary = function(trueA, trueB, trueSd, burnIn, chain, num_class = 30){
+  par(mfrow = c(2,3))
+  hist(chain[-(1:burnIn),1],nclass=num_class, , main="Posterior of a", xlab="True value = red line" )
+  abline(v = mean(chain[-(1:burnIn),1]))
+  abline(v = trueA, col="red" )
+  hist(chain[-(1:burnIn),2],nclass=num_class, main="Posterior of b", xlab="True value = red line")
+  abline(v = mean(chain[-(1:burnIn),2]))
+  abline(v = trueB, col="red" )
+  hist(chain[-(1:burnIn),3],nclass=num_class, main="Posterior of sd", xlab="True value = red line")
+  abline(v = mean(chain[-(1:burnIn),3]) )
+  abline(v = trueSd, col="red" )
+  plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of a", )
+  abline(h = trueA, col="red" )
+  plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of b", )
+  abline(h = trueB, col="red" )
+  plot(chain[-(1:burnIn),3], type = "l", xlab="True value = red line" , main = "Chain values of sd", )
+  abline(h = trueSd, col="red" )
+}
 
+plot_summary(trueA, trueB, trueSd, burnIn, chain, num_class = 30)
 # for comparison:
 summary(lm(y~x))
 
